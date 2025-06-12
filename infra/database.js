52001cc7cc420fd495eec6077b92f5eb2f68daa6
Miguel Attie <attiemiguel751@gmail.com>
2025-06-12 05:03:10 -0300

    add function queries `prev SQL Injection` from `pg`

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,29 @@
-import {Client} from 'pg';
+import { Client } from "pg";
 
-async function query(queryObject){
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD
-    });
-    await client.connect();
-    const result = await client.query(queryObject); 
-    await client.end();
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Utilizo as funções de query do pg para realizar as consultas no banco de dados com prevenção de SQL Injection
+  try {
+    const result = await client.query(queryObject);
     return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
-
-export default{
-    query: query,
-}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+export default {
+  query: query,
+};
 
 // psql --host=localhost --port=5432 --username=postgres --password=test
