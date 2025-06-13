6a2f0718b11a9908c7e4f66b74ce1c2217d66364
Miguel Attie <attiemiguel751@gmail.com>
2025-06-13 15:49:50 -0300

    `client.connect()` mais robusto contra erros no `database.js`

--- infra/database.js
+++ infra/database.js
@@ -8,15 +8,23 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("Requisições database:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  } )
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // Utilizo as funções de query do pg para realizar as consultas no banco de dados com prevenção de SQL Injection
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
