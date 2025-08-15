7c0c4d6c799226a0c3f249bc2a2eebbc62d7c9e9
Miguel Attie <attiemiguel751@gmail.com>
2025-08-15 14:04:33 -0300

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,19 +1,11 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-
+  let client;
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // Utilizo as funções de query do pg para realizar as consultas no banco de dados com prevenção de SQL Injection
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -23,9 +15,22 @@ async function query(queryObject) {
     await client.end();
   }
 }
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  });
+  await client.connect();
+  return client;
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
-
-// psql --host=localhost --port=5432 --username=postgres --password=test
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv"); // Por padrão o next/jest em variaveis de ambiente não localiza .development
+dotenv.config({
+  path: ".env.development", //config dotenv
+});
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig();
