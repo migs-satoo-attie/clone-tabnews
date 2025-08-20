80fa10815860e82179adfc99997e733e21a238d1
Miguel Attie <attiemiguel751@gmail.com>
2025-08-20 18:52:29 -0300

    makes `npm run test` more robust with `async-retry` and `orquestrator.js`

--- tests/integration/api/v1/migrations/POST.test.js
+++ tests/integration/api/v1/migrations/POST.test.js
@@ -1,10 +1,10 @@
 import database from "infra/database";
+import orquestrator from "tests/orquestrator.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orquestrator.waitForAllServices();
   await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-}
+});
 
 test("POST to api/v1/migrations deve retornar 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
